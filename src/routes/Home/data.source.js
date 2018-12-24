a4774815cebe3976d5068f8cdf1b777a6ea4a1aa
conzhang <zhangkunyong@cetcbigdata.ocm>
2018-12-24 15:38:21 +0800

    初始化

---
+++ src/routes/Home/data.source.js
@@ -0,0 +1,276 @@
+import React from 'react';
+export const Nav20DataSource = {
+  isScrollLink: true,
+  wrapper: { className: 'header2 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header2-logo',
+    children: 'https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg',
+  },
+  Menu: {
+    className: 'header2-menu',
+    children: [
+      {
+        name: 'Banner0_0',
+        to: 'Banner0_0',
+        children: 'Banner0_0',
+        className: 'menu-item',
+      },
+      {
+        name: 'Content0_0',
+        to: 'Content0_0',
+        children: 'Content0_0',
+        className: 'menu-item',
+      },
+      {
+        name: 'Content1_0',
+        to: 'Content1_0',
+        children: 'Content1_0',
+        className: 'menu-item',
+      },
+    ],
+  },
+  mobileMenu: { className: 'header2-mobile-menu' },
+  menuLink: { children: [] },
+};
+export const Banner20DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0' },
+        title: { className: 'banner2-title', children: 'Ant Motion' },
+        content: {
+          className: 'banner2-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner2-button', children: 'Learn More' },
+      },
+    ],
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: '产品与服务' }],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+          },
+          title: { className: 'content0-title', children: '一站式业务接入' },
+          content: { children: '支付、结算、核算接入产品效率翻四倍' },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: '一站式事中风险监控',
+          },
+          content: { children: '在所有需求配置环节事前风险控制和质量控制能力' },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+          },
+          title: { className: 'content0-title', children: '一站式数据运营' },
+          content: { children: '沉淀产品接入效率和运营小二工作效率数据' },
+        },
+      },
+    ],
+  },
+};
+export const Content11DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: { className: 'content1-title', children: '企业资源管理' },
+  content: {
+    className: 'content1-content',
+    children:
+      '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+  },
+};
+export const Content20DataSource = {
+  wrapper: { className: 'home-page-wrapper content2-wrapper' },
+  OverPack: { className: 'home-page content2', playScale: 0.3 },
+  imgWrapper: { className: 'content2-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/tvQTfCupGUFKSfQ.png',
+  },
+  textWrapper: { className: 'content2-text', md: 14, xs: 24 },
+  title: { className: 'content2-title', children: '分布式中间件' },
+  content: {
+    className: 'content2-content',
+    children:
+      '金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。',
+  },
+};
+export const Content12DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: { className: 'content1-title', children: '企业资源管理' },
+  content: {
+    className: 'content1-content',
+    children:
+      '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/qqaimmXZVSwAhpL.svg',
+        },
+        content: {
+          className: 'slogan',
+          children: 'Animation specification and components of Ant Design.',
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '产品' },
+        content: {
+          children: (
+            <span>
+              <p>
+                {' '}
+                <a href="#">产品更新记录</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">API文档</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">快速入门</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">参考指南</a>{' '}
+              </p>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '关于' },
+        content: {
+          children: (
+            <span>
+              <p>
+                {' '}
+                <a href="#">FAQ</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">联系我们</a>{' '}
+              </p>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '资源' },
+        content: {
+          children: (
+            <span>
+              <p>
+                {' '}
+                <a href="#">Ant Design</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">Ant Design</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">Ant Design</a>{' '}
+              </p>{' '}
+              <p>
+                {' '}
+                <a href="#">Ant Design</a>{' '}
+              </p>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        ©2018 by <a href="https://motion.ant.design">Ant Motion</a> All Rights
+        Reserved
+      </span>
+    ),
+  },
+};
